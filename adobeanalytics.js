ae7a98901a534a22973fa58552802a7613514dda
Dave Stein <dave@behance.com>
2014-04-01 12:08:31 -0400

    Adobe Analytics: Try catching

--- adobeanalytics.js
+++ adobeanalytics.js
@@ -2,17 +2,28 @@
  * Adobe Analytics tracking. s_adobe comes from their script
  */
 /*global s_adobe */
-define({
-  page: function() {
-    // Safeguard in case module is loaded without script on page
-    if (typeof s_adobe === 'undefined') {
-      return;
+define(['log'], function(log) {
+  'use strict';
+
+  log = log.get('adobeanalytics');
+
+  return {
+    page: function() {
+      // Safeguard in case module is loaded without script on page
+      if (typeof s_adobe === 'undefined') {
+        return;
+      }
+
+      try {
+        s_adobe.pageName = (window.location.hostname + window.location.pathname)
+                            .replace(/\//g, ':')
+                            .replace(/^www\./, '');
+
+        s_adobe.t();
+      }
+      catch(e) {
+        log.error(e);
+      }
     }
-
-    s_adobe.pageName = (window.location.hostname + window.location.pathname)
-                        .replace(/\//g, ':')
-                        .replace(/^www\./, '');
-
-    s_adobe.t();
-  }
+  };
 });
